aac0c5f42c6b10e63b2532a717a23b3c27ebd900
“Wendy <wendysjkim@gmail.com>
2019-07-05 18:43:52 -0400

    finished lab

--- school.py
+++ school.py
@@ -1,1 +1,20 @@
 class School:
+  def __init__(self, name, roster={}):
+    self.name = name
+    self.roster = roster
+
+  def add_student(self, student_name, grade):
+    if grade in self.roster.keys():
+      self.roster[grade].append(student_name)
+    else:
+      self.roster[grade] = [student_name]
+
+  def grade(self, grade):
+    return self.roster[grade] 
+
+  def sort_roster(self):
+    new_roster = self.roster
+    for key in new_roster:
+      new_roster[key].sort()
+    return new_roster  
+   
